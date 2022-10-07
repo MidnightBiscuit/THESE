2ebe5a13f3ef9ba912855027c0d37ecd102fff44
MidnightBiscuit <aldehyde@hotmail.fr>
2022-10-07 17:50:46 +0200

    progres on RF study correction on errors on computation of UDC for alpha=1

--- RF_Temp_Fit/functions.py
+++ RF_Temp_Fit/functions.py
@@ -98,3 +98,89 @@ def load_x_afterstart(filename):
 
 	xva_trj1 = reshape(xva_trj1, (6, junk[0]//8//6),order='F')
 	return xva_trj1
+
+def load_xyz_init_bin_DP(str_load):
+    fid = open(str_load+'.bin',  'rb')   
+    a    = fromfile(fid,  int32, 1)        # Read record start tag   
+    aux  = fromfile(fid,  int32, 1)   
+    junk = fromfile(fid,  int32, 1)        # Read record stop tag   
+    N_ions = aux[0]   
+
+    b    = fromfile(fid,  int32, 1)        # Read record start tag   
+    aux  = fromfile(fid,  int32, 1)   
+    junk = fromfile(fid,  int32, 1)        # Read record stop tag   
+    j_save = aux[0]     
+
+    d    = fromfile(fid,  int32, 1)        # Read record start tag   
+    dt   = fromfile(fid,  float64, 1)   
+    junk = fromfile(fid,  int32, 1)        # Read record stop tag
+    
+    e    = fromfile(fid,  int32,   1   )        # Read record start tag   
+    iRF  = fromfile(fid,  int32, 3)   
+    junk = fromfile(fid,  int32,   1   )        # Read record stop tag   
+
+    f    = fromfile(fid,  int32,   1   )        # Read record start tag 
+    # ~ print(f)
+    save_xva = fromfile(fid,  float64, 12*N_ions)   
+    junk = fromfile(fid,  int32,   1   )        # Read record stop tag
+    # ~ print(junk)
+
+    fid.close
+    save_xva = reshape(save_xva, (12,N_ions), order='F')
+
+    return save_xva
+
+def plot_XYZ(file_name,fig_name='2',fig_title='XYZ'):
+    save_xva = load_xyz_init_bin_DP(file_name)
+    r_LC = save_xva[0:3,:] 
+    v_LC = save_xva[3:6,:]
+    a_LC = save_xva[6:9,:]
+    v_LC_avg = save_xva[9:,:]
+    # ~ print(r_LC)
+    figure(fig_name); clf()
+    title(fig_title)
+    subplot(211)
+    plot(r_LC [0,:]*1e6,r_LC [1,:]*1e6,'8',color='xkcd:purplish blue')
+    xlabel('x [µm]')
+    ylabel('y [µm]')
+    grid()
+
+    # subplot(212,aspect=1.0)
+    subplot(212,aspect=1.0)
+    plot(r_LC [2,:]*1e6,r_LC [1,:]*1e6,'8',color='xkcd:purplish blue')
+    xlabel('z [µm]')
+    ylabel('y [µm]')
+    grid()
+    
+    tight_layout()
+
+def load_xva_init_bin_Lan(str_load, flag_print):      
+    fid = open(str_load+'.bin',  'rb')   
+    a    = fromfile(fid,  int32, 1)        # Read record start tag   
+    aux  = fromfile(fid,  int32, 1)   
+    junk = fromfile(fid,  int32, 1)        # Read record stop tag   
+    N_ions = aux[0]   
+
+    b    = fromfile(fid,  int32, 1)        # Read record start tag   
+    aux  = fromfile(fid,  int32, 1)   
+    junk = fromfile(fid,  int32, 1)        # Read record stop tag   
+    j_save = aux[0]     
+
+    d    = fromfile(fid,  int32, 1)        # Read record start tag   
+    dt   = fromfile(fid,  float64, 1)   
+    junk = fromfile(fid,  int32, 1)        # Read record stop tag
+    
+    e    = fromfile(fid,  int32,   1   )        # Read record start tag   
+    iRF  = fromfile(fid,  int32, 3)   
+    junk = fromfile(fid,  int32,   1   )        # Read record stop tag   
+
+    f    = fromfile(fid,  int32,   1   )        # Read record start tag 
+    # ~ print(f)
+    save_xva = fromfile(fid,  float64, 12*N_ions)   
+    junk = fromfile(fid,  int32,   1   )        # Read record stop tag
+    # ~ print(junk)
+
+    fid.close
+    save_xva = reshape(save_xva, (12,N_ions), order='F')
+
+    return N_ions, j_save, dt, iRF, save_xva
